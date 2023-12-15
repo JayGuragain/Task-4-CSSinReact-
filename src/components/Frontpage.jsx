2526552bf726d8a8a8003fda981e99c2d1f00169
JayGuragain <bijaya.uniglobe@gmail.com>
2023-12-14 22:53:29 -0800

    Initial commit

--- src/components/Frontpage.jsx
+++ src/components/Frontpage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Frontpage = () => {
   return (
