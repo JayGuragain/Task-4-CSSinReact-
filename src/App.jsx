169a86d601406aabc7e77ea324378a3881b6e878
JayGuragain <bijaya.uniglobe@gmail.com>
2023-12-14 22:43:03 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// App.js
+
+import React from "react";
+import "./App.css";
+import Frontpage from "./components/Frontpage";
+// Adjust the path as necessary
+
+function App() {
+  return (
+    <div>
+      <Frontpage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Frontpage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+const Frontpage = () => {
+  return (
+    <div className="main-container">
+      <div className="card">
+        <div className="card-image">
+          <img src="/perfume.avif" alt="Perfume" />
+        </div>
+
+        <div className="card-title">
+          <h2>PERFUME</h2>
+          <h1>GABRIELLE ESSENCE Eau De Perfum</h1>
+
+          <p className="card-description">
+            A floral, solar and voluptuous interpretation composed by Oliver
+            Polgo, Perfumer-Creator for the House of CHANEL.
+          </p>
+
+          <p className="card-price">
+            $149.99 <span className="card-price-original">$169.99</span>
+          </p>
+
+          <button className="card-button">
+            <FontAwesomeIcon icon={faShoppingCart} />
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Frontpage;
